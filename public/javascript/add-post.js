7fd52383e350d6643f8c3aa1cf66bb5b17874560
Ward <wardv@tirf.ca>
2022-02-27 12:20:42 -0500

    changed edit post input to textarea

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,6 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
+    // const post_text = document.querySelector('textarea[name="post-text"]').textContent;
     const post_text = document.querySelector('input[name="post-text"]').value;
   
     const response = await fetch(`/api/posts`, {
