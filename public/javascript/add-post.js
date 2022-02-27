25388b51f72bfd2d215148dcf02a06e9f923fdc3
Ward <wardv@tirf.ca>
2022-02-27 12:28:31 -0500

    finished some styling

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,8 +2,8 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    // const post_text = document.querySelector('textarea[name="post-text"]').textContent;
-    const post_text = document.querySelector('input[name="post-text"]').value;
+    const post_text = document.querySelector('textarea[name="post-text"]').value;
+    // const post_text = document.querySelector('input[name="post-text"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
